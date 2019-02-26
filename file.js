4c0cd3aeba4011020623542f88d9f96cca1334b9
seriouslag <Landon.Gavin@gmail.com>
2019-02-26 16:27:20 -0500

    reduced jquery need, added comments, performance tuning

--- file.js
+++ file.js
@@ -1,119 +1,93 @@
 // Inital user value state
-var user = null;
-// Gets a reference to our firebase realtime database instance
-var database = firebase.database();
+let user = null;
 
-$(document).ready(function () {
-    $(".btn-primary").click(function () {
-        $(this).text(function (i, text) {
-            return text === "Hide the Party Members" ? "See the Party Members" : "Hide the Party Members";
-        })
-    });
+// Storing commonly used elements for reference
+const elements = {
+    showParty: document.getElementById("show-party"),
+    loginBtn: document.getElementById("loginBtn"),
+    logoutBtn: document.getElementById("logoutBtn"),
+    loginForm: document.getElementById("loginForm"),
+    loginModal: document.getElementById("loginModal"),
 
-    // Register event handler for the submit action of the login form
-    $("#loginForm").on('submit', handleLoginFormSubmit);
+    emailInput: document.querySelector("#loginForm input[name=email]"),
+    passwordInput: document.querySelector("#loginForm input[name=password]"),
+    partyDeck: document.querySelector("#party.card-deck")
+};
 
-    // Register event handler for the logout button
-    $("#logoutBtn").on('click', signOut);
-});
+/*
+    Register event handlers
+*/
+elements.showParty.addEventListener("click", handleShowPartyClick);
 
-function showLoggedOutUi() {
-    $("#loginBtn").removeClass("d-none");
-    $("#logoutBtn").addClass("d-none");
+// Register event handler for the submit action of the login form
+elements.loginForm.addEventListener("submit", handleLoginFormSubmit);
+
+// Register event handler for the logout button
+elements.logoutBtn.addEventListener("click", signOut);
+
+/*
+    End Registering event handlers
+*/
+
+/*
+    Event handlers
+*/
+function handleShowPartyClick($event) {
+    var btn = $event.currentTarget;
+    var innerHtml = btn.innerHTML;
+    var newText = innerHtml === "Hide the Party Members" ? "See the Party Members" : "Hide the Party Members";
+
+    btn.innerHTML = newText;
 }
-
-function showLoggedInUi() {
-    $("#loginBtn").addClass("d-none");
-    $("#logoutBtn").removeClass("d-none");
-}
-
-
-// This is an observable added by firebase
-// Here we are registering functions to be called on the 'onAuthStateChanged' event of firebase.auth
-// https://firebase.google.com/docs/reference/js/firebase.auth.Auth?authuser=0#onAuthStateChanged
-firebase.auth().onAuthStateChanged(function (user) {
-    // New user value
-    // if null then user has logged out
-    this.user = user;
-    console.log('User value has changed!', user);
-    if (user) {
-        // User is signed in.
-        //alert('You are logged in to ' + user.email + "!");
-
-        $('#loginModal').modal('hide')
-
-        // show logged in ui // hide logged out ui
-        showLoggedInUi();
-    } else {
-        // No user is signed in.
-        // hide logged in ui / show logged out ui
-        showLoggedOutUi();
-    }
-});
 
 function handleLoginFormSubmit($event) {
-    // This prevents the forms submit action from reloading the page/calling the action
+    // This prevents the forms submit action from reloading the page/calling the form's action
     $event.preventDefault();
 
     // Getting form values
-    var emailInput = $("#loginForm input[name=email]");
-    var email = emailInput.val();
-    var passwordInput = $("#loginForm input[name=password]");
-    var password = passwordInput.val();
+    var emailInput = elements.emailInput;
+    var email = emailInput.value;
+    var passwordInput = elements.passwordInput;
+    var password = passwordInput.value;
 
     // Calling function to login with provided values
     signInWithEmailAndPassword(email, password);
 }
 
 function signInWithEmailAndPassword(email, password) {
-    // Firebase sign in with email and password
-    // https://firebase.google.com/docs/reference/js/firebase.auth.Auth?authuser=0#isSignInWithEmailLink
-    firebase.auth().signInWithEmailAndPassword(email, password)
-        .then(function (user) {
-            // logged in
-            // Here we could handle any logic we need with the new user value that is returned
-            // Instead we are letting the event handler we registered for firebase's 'onAuthStateChanged' to handle it
-        })
-        .catch(function (error) {
-            // Handle login errors here
-            var errorCode = error.code;
-            var errorMessage = error.message;
-            console.log('Failed to log in', error);
-            alert(errorMessage);
-        });
+    firebaseSignInWithEmailAndPassword(email, password);
 }
 
 function signOut() {
-    // Signs the user out
-    // https://firebase.google.com/docs/reference/js/firebase.auth.Auth?authuser=0#signOut
-    firebase.auth().signOut();
+    firebaseSignOut();
 }
 
-// place is database we plan to attach a observer too
-var usersRef = firebase.database().ref('users/');
+/*
+    End event handlers
+*/
 
-// database observer, this will call the handleUserUpdate
-// when data is changed/first time it is bound
-usersRef.on('value', handleUsersUpdate);
+function showLoggedOutUi() {
+    removeClass(elements.loginBtn, "d-none");
+    addClass(elements.logoutBtn, "d-none");
+}
 
-function handleUsersUpdate(snapshot) {
-    // snapshot is the object that is returned from firebase
-    // calling the .val() method on the snapshot returns the 
-    // values from the database as a javascript object
-    var users = snapshot.val();
-
-    // pass the array of users to build the character cards function
-    buildCharacterCards(users);
-};
+function showLoggedInUi() {
+    // we have to wrap in jquery selector 
+    // so that we can call the bootstrap function modal;
+    // jquery needs to be loaded before this will work
+    $(elements.loginModal).modal('hide');
+    addClass(elements.loginBtn, "d-none");
+    removeClass(elements.logoutBtn, "d-none");
+}
 
 function buildCharacterCards(users) {
     // Empty out card container so if data is changed we are not
     // creating the same cards/modals again
-    $("#party.card-deck").empty();
+    elements.partyDeck.innerHTML = "";
     // iterate of the list of users
-    $.each(users, function (id, user) {
+    users.forEach(function (user) {
         // iterate of each character a user has
-        $.each(user.characters, function (index, character) {
+        user.characters.forEach(function (character) {
             // build the character card
             buildCharacterCard(character);
             // build the character modal
@@ -124,17 +98,17 @@ function buildCharacterCards(users) {
 
 function buildCharacterCard(character) {
     // Get the character props
-    var id = character.id;
-    var name = character.name;
-    var level = character.level;
-    var motto = character.motto;
-    var description = character.description;
-    var image = character.image;
-    var title = character.title;
-    var characterClass = character.class;
+    const id = character.id;
+    const name = character.name;
+    const level = character.level;
+    const motto = character.motto;
+    const description = character.description;
+    const image = character.image;
+    const title = character.title;
+    const characterClass = character.class;
 
     // Build the card string
-    var cardString =
+    const cardString =
         '<div class="card">' +
         '<img class="card-img-top" src="' + image + '" alt="' + title + '">' +
         '<div class="card-body text-center">' +
@@ -145,22 +119,25 @@ function buildCharacterCard(character) {
         '</div>';
 
     // Append the new card to the card deck container
-    $("#party.card-deck").append(cardString);
+    //$("#party.card-deck").append(cardString);
+
+    const nodes = getDomNodesFromString(cardString);
+    appendChildrenNodes(elements.partyDeck, nodes);
 }
 
 function buildCharacterModal(character) {
     // Get the character props
-    var id = character.id;
-    var name = character.name;
-    var level = character.level;
-    var motto = character.motto;
-    var description = character.description;
-    var image = character.image;
-    var title = character.title;
-    var characterClass = character.class;
+    const id = character.id;
+    const name = character.name;
+    const level = character.level;
+    const motto = character.motto;
+    const description = character.description;
+    const image = character.image;
+    const title = character.title;
+    const characterClass = character.class;
 
     // Build the modal string
-    var modalString =
+    let modalString =
         '<div class="modal fade" id="' + name + 'Modal" tabindex="-1" role="dialog" aria-labelledby="modalCenterTitle" aria-hidden="true">' +
         '<div class="modal-dialog modal-dialog-centered" role="document">' +
         '<div class="modal-content">' +
@@ -173,7 +150,7 @@ function buildCharacterModal(character) {
         '<div class="modal-body">';
 
     // Loop over each description and add it to the modal string
-    $.each(description, function (index, value) {
+    description.forEach(function (index, value) {
         modalString += '<p>' + value + '</p>';
     });
 
@@ -191,5 +168,135 @@ function buildCharacterModal(character) {
     // to attach;
     // When we generate new cards we first empty the card deck so all
     // the modal gets emptied as well
-    $("#party.card-deck").append(modalString);
+    const nodes = getDomNodesFromString(modalString);
+    appendChildrenNodes(elements.partyDeck, nodes);
 }
+
+/*
+    Helper functions
+*/
+
+function getDomNodesFromString(str) {
+    const nodes = new DOMParser().parseFromString(str, 'text/html');
+    const htmlNode = nodes.childNodes[0];
+    const bodyNode = htmlNode.childNodes[1];
+    const content = bodyNode.childNodes;
+    return content;
+}
+
+function appendChildrenNodes(element, arrayOfNodes) {
+    arrayOfNodes.forEach(function (node) {
+        element.appendChild(node);
+    })
+}
+
+function addClass(element, className) {
+    element.classList.add(className);
+}
+
+function removeClass(element, className) {
+    element.classList.remove(className);
+}
+
+/*
+    End helper functions
+*/
+
+/*
+    Firebase
+*/
+
+var config = {
+    apiKey: "AIzaSyCrTQ1lP-zuy4qlPzGYY-CDMALbOJnU1Fg",
+    authDomain: "gloom-36e1b.firebaseapp.com",
+    databaseURL: "https://gloom-36e1b.firebaseio.com",
+    projectId: "gloom-36e1b",
+    storageBucket: "gloom-36e1b.appspot.com",
+    messagingSenderId: "667862872382"
+};
+firebase.initializeApp(config);
+
+// Gets a reference to our firebase realtime database instance
+const database = firebase.database();
+
+// reference to location in database we are going to attach a handler to
+var usersRef = firebase.database().ref('users/');
+
+// This is an observable added by firebase
+// Here we are registering functions to be called on the 'onAuthStateChanged' event of firebase.auth
+// https://firebase.google.com/docs/reference/js/firebase.auth.Auth?authuser=0#onAuthStateChanged
+firebase.auth().onAuthStateChanged(onAuthStateChanged);
+
+function firebaseSignInWithEmailAndPassword(email, password) {
+    // Firebase sign in with email and password
+    // https://firebase.google.com/docs/reference/js/firebase.auth.Auth?authuser=0#isSignInWithEmailLink
+    firebase.auth().signInWithEmailAndPassword(email, password)
+        .then(signInWithEmailAndPasswordSuccessHandler)
+        .catch(signInWithEmailAndPasswordErrorHandler);
+}
+
+function firebaseSignOut() {
+    // Signs the user out
+    // https://firebase.google.com/docs/reference/js/firebase.auth.Auth?authuser=0#signOut
+    firebase.auth().signOut();
+}
+
+// database observer, this will call the handleUserUpdate
+// when data is changed/first time it is bound
+usersRef.on('value', handleUsersUpdate);
+
+/*
+    Firebase event handlers
+*/
+
+function onAuthStateChanged(user) {
+    // New user value
+    // if null then user has logged out
+    // this.user is a reference to the global user variable
+    this.user = user;
+    console.log('User value has changed!', user);
+    if (user) {
+        // User is signed in.
+        // show logged in ui // hide logged out ui
+        showLoggedInUi();
+    } else {
+        // No user is signed in.
+        // hide logged in ui / show logged out ui
+        showLoggedOutUi();
+    }
+}
+
+function handleUsersUpdate(snapshot) {
+    // snapshot is the object that is returned from firebase
+    // calling the .val() method on the snapshot returns the 
+    // values from the database as a javascript object
+
+    // .val() is a method added by firebase
+    // snapshot.val() returns an object with the properties being the items id
+    // we don't want that; we want an array with all the values
+    // Object.values() gives us that
+    const users = Object.values(snapshot.val());
+
+    // pass the array of users to build the character cards function
+    buildCharacterCards(users);
+};
+
+function signInWithEmailAndPasswordSuccessHandler(user) {
+    // We are letting the onAuthStateChanged event handle the login events
+}
+
+function signInWithEmailAndPasswordErrorHandler(error) {
+    // Handle login errors here
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    console.log('Failed to log in', error);
+    alert(errorMessage);
+}
+
+/*
+    End firebase event handlers
+*/
+
+/*
+    End firebase
+*/
